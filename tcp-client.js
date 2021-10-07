2380525227bacf1d8f7afcea5ce4750d0bbbe268
Paul Frazee <pfrazee@gmail.com>
2021-10-07 09:30:17 -0500

    Add host env to tcp-client

--- tcp-client.js
+++ tcp-client.js
@@ -1,10 +1,11 @@
 import net from 'net'
 
+const host = process.env.HOST
 const port = Number(process.env.PORT)
 
 function roundtrip (port) {
   return new Promise((resolve) => {
-    const client = net.createConnection({ port }, () => {
+    const client = net.createConnection({ host, port }, () => {
       client.write(Buffer.from([1,2,3,4,5]))
       client.on('data', chunk => {
         console.log(chunk)
